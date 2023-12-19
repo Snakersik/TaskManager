b67b189ca452e959456fe5551577ba4fc9ace09f
Snakersik <konradtratwal@gmail.com>
2023-12-19 19:18:10 +0100

    FirstSetup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>taskManager</h1>
+      <div className="TaskBox">
+        <Tasks />
+        <Tasks />
+        <Tasks />
+        <Tasks />
+      </div>
     </div>
   );
 }
+
+const Tasks = () => {
+  return (
+    <div className="taskBox">
+      <h2>Task</h2>
+      <p>Task Desc</p>
+    </div>
+  );
+};
 
 export default App;
